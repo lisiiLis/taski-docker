c73332ead4cd367d1d29e5400a30ba598c11c148
Victor Minets <punk2495@mail.ru>
2026-01-20 18:36:36 +0300

    settings edit, HOST

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '158.160.94.69']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'planing.servebeer.com', '158.160.94.69']
 
 
 # Application definition
